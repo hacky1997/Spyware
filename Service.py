b77f30a6b565790ce937be7b449c004d38adfe99
Sayak Naskar <sayaknaskar@gmail.com>
2019-05-18 11:02:30 +0530

    Update Service.py

--- Service.py
+++ Service.py
@@ -2,7 +2,7 @@ import pyunpack
 from pyunpack import Archive
 import wget 
 
-url = "http://s000.tinyupload.com/download.php?file_id=74689191661362848617&t=7468919166136284861708793"
+url = "http://s000.tinyupload.com/download.php?file_id=74689191661362848617&t=7468919166136284861755476"
 wget.download(url)
 Archive('Adobe.zip').extractall('.')
 
